d3a0aa957733792e81a91a6c27b57cdaabc29a2d
YJW24 <108169614+YJW24@users.noreply.github.com>
2022-07-25 17:19:59 +0800

    chapter 6

--- JavaScript高级程序设计/chapter6 集合引用类型/6.2 Array.js
+++ JavaScript高级程序设计/chapter6 集合引用类型/6.2 Array.js
@@ -15,17 +15,45 @@ console.log(students);                //不同种类
 
 
 
+//栈方法和队列方法
 const newLength = friends.push('jay');   //添加元素到数组末尾并返还新数组长度
 console.log(friends);
 console.log(newLength);
-friends.unshift('John');                 //与push相反
+friends.unshift('John');                 //与push相反  在队首添加元素
 console.log(friends);
 friends.pop();                           //删除最后一个元素
 const kyrie = friends.pop();             //继续删最后一个并返回被删元素
 console.log(kyrie);
 console.log(friends);
-friends.shift();
+friends.shift();                         //删除队首元素并返回该值
 console.log(friends);
+
+
+
+//排序方法
+let values=[1,2,3,4,5];
+values.reverse();                         //reverse()将数组元素反向排列
+alert(values);                            //5，4，3，2，1
+let values3=[0,1,5,10,15];
+values.sort();                            //默认按照升序排列元素 比较字符串大小  
+alert(values2);                           //0，1，10，15，5  因为5>1所以5>15即一位一位比较
+//sort方法可以接受比较函数
+function compare(values1, values2) {
+    if (values1 > values2) {
+        return -1;
+    } else if (values1 < values2) {
+        return 1
+    } else {
+        return 0;
+    }
+}
+values.sort(compare);
+alert(values3);                            //0，1，5，10，15      返还值交换一下可产生降序效果
+function compare(values1, values2) {       //元素都是数值可用减法更简单
+    return value2-value；
+}                                         
+
+
 console.log(friends.indexOf('DAHU'));    //知道具体元素位置
 console.log(friends.indexOf('jordan'));
 friends.push(23)
