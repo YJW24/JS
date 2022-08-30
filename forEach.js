2f5b318c7b9f42a0f95453c83219a3d16fdc24a6
YJW24 <108169614+YJW24@users.noreply.github.com>
2022-08-30 10:43:32 +0800

    Update forEach.js

--- forEach.js
+++ forEach.js
@@ -1,11 +1,11 @@
-let hanShu = function (Object) {
-    console.log(Object + 2)
+let func = function (obj) {
+    console.log(obj + 2)
 }
 
-let eachFor = function (Array, Object) {
-    for (let i = 0; i < Array.length; i++) {
-        Object(Array[i]);
+let eachFor = function (array, obj) {
+    for (let i = 0; i < array.length; i++) {
+        obj(array[i]);
     }
 }
 
-eachFor(["2", 2, 4], hanShu);
+eachFor(["2", 2, 4], func);
